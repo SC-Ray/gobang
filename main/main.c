113746370681839de7ff3b9ab48f491d8f5270dc
Scray <2487298826@qq.com>
2024-09-16 20:51:54 +0800

    finish pvp1.0

--- main/main.c
+++ main/main.c
@@ -14,7 +14,10 @@ int main(const int argument_count, char *const argument_value[])
     int y;
     int x;
     int man;
+    int round;
     chessboard_print();
+    printf("Please ask the while side to play chess\n");
+    printf("(y x 1)\n");
     scanf("%d %d %d", &y, &x, &man);
     chessboard_set_unit(y, x, man);
     chessboard_print();
@@ -22,10 +25,48 @@ int main(const int argument_count, char *const argument_value[])
 
     while (0 != chessboard_determine(y, x))
     {
-        scanf("%d %d %d", &y, &x, &man);
-        chessboard_set_unit(y, x, man);
-        chessboard_print();
-        LOG_MESSAGE("[%s: %d] %s\n", GET_FILE_NAME(__FILE__), __LINE__, "print");
+        for (round = 1; round < size * size; ++round)
+        {
+            if (1 == round % 2)
+            {
+                printf("turn to black side play chess\n");
+                printf("(y x 2)\n");
+                scanf("%d %d %d", &y, &x, &man);
+                if (y > size || x > size)
+                {
+                    printf("You have exceeded the maximum range,please print again!\n");
+                    scanf("%d %d %d", &y, &x, &man);
+                    chessboard_set_unit(y, x, man);
+                    chessboard_print();
+                }
+                else
+                {
+                    chessboard_set_unit(y, x, man);
+                    chessboard_print();
+                }
+            }
+
+            if (0 == round % 2)
+            {
+                printf("turn to while side play chess\n");
+                printf("(y x 1)\n");
+                scanf("%d %d %d", &y, &x, &man);
+                if (y > size || x > size)
+                {
+                    printf("You have exceeded the maximum range,please print again!\n");
+                    scanf("%d %d %d", &y, &x, &man);
+                    chessboard_set_unit(y, x, man);
+                    chessboard_print();
+                }
+                else
+                {
+                    chessboard_set_unit(y, x, man);
+                    chessboard_print();
+                }
+            }
+        }
+
+        // LOG_MESSAGE("[%s: %d] %s\n", GET_FILE_NAME(__FILE__), __LINE__, "print");
     }
 
     return 0;
