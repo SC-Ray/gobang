3247038cf980cc9b4deace698e0d13b66dc06cae
Scray <2487298826@qq.com>
2024-09-16 21:46:10 +0800

    finish pvp2.0

--- main/main.c
+++ main/main.c
@@ -11,62 +11,42 @@ int main(const int argument_count, char *const argument_value[])
     // chessboard initialize
     chessboard_initialize(size);
 
-    int y;
-    int x;
-    int man;
-    int round;
-    chessboard_print();
-    printf("Please ask the while side to play chess\n");
-    printf("(y x 1)\n");
-    scanf("%d %d %d", &y, &x, &man);
-    chessboard_set_unit(y, x, man);
-    chessboard_print();
-    LOG_MESSAGE("[%s: %d] %s\n", GET_FILE_NAME(__FILE__), __LINE__, "print");
-
-    while (0 != chessboard_determine(y, x))
+    for (unsigned int round = 0; round < size * size; ++round)
     {
-        for (round = 1; round < size * size; ++round)
+        int y, x, man;
+        chessboard_print();
+        if (1 == round % 2)
         {
-            if (1 == round % 2)
+            printf("turn to black side play chess\n");
+            printf("(y x 2)\n");
+            scanf("%d %d %d", &y, &x, &man);
+            if (y > size - 1 || x > size - 1)
             {
-                printf("turn to black side play chess\n");
-                printf("(y x 2)\n");
+                printf("You have exceeded the maximum range,please print again!\n");
                 scanf("%d %d %d", &y, &x, &man);
-                if (y > size || x > size)
-                {
-                    printf("You have exceeded the maximum range,please print again!\n");
-                    scanf("%d %d %d", &y, &x, &man);
-                    chessboard_set_unit(y, x, man);
-                    chessboard_print();
-                }
-                else
-                {
-                    chessboard_set_unit(y, x, man);
-                    chessboard_print();
-                }
+                chessboard_set_unit(y, x, man);
             }
-
-            if (0 == round % 2)
+            else
             {
-                printf("turn to while side play chess\n");
-                printf("(y x 1)\n");
-                scanf("%d %d %d", &y, &x, &man);
-                if (y > size || x > size)
-                {
-                    printf("You have exceeded the maximum range,please print again!\n");
-                    scanf("%d %d %d", &y, &x, &man);
-                    chessboard_set_unit(y, x, man);
-                    chessboard_print();
-                }
-                else
-                {
-                    chessboard_set_unit(y, x, man);
-                    chessboard_print();
-                }
+                chessboard_set_unit(y, x, man);
             }
         }
-
-        // LOG_MESSAGE("[%s: %d] %s\n", GET_FILE_NAME(__FILE__), __LINE__, "print");
+        else
+        {
+            printf("turn to while side play chess\n");
+            printf("(y x 1)\n");
+            scanf("%d %d %d", &y, &x, &man);
+            if (y > size - 1 || x > size - 1)
+            {
+                printf("You have exceeded the maximum range,please print again!\n");
+                scanf("%d %d %d", &y, &x, &man);
+                chessboard_set_unit(y, x, man);
+            }
+            else
+            {
+                chessboard_set_unit(y, x, man);
+            }
+        }
     }
 
     return 0;
